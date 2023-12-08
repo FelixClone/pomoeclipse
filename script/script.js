fd67de38a1a23725ee5d61efa0f2110529c87547
Rodrigo Felix <rodrigofx.dev@gmail.com>
2023-12-08 06:07:33 -0300

    :recycle: refactor: some comments have been added in situations where the names of functions and variables do not explain themselves

--- script/script.js
+++ script/script.js
@@ -1,30 +1,30 @@
-let isRunning = false;
-let timer;
-
 let allCir = document.querySelectorAll('.bi-circle-fill');
 let wheel = document.getElementById('wheelTime');
 let playerPause = document.getElementById('playerPause');
 let audReset = document.getElementById('audReset');
 let audStartStop = document.getElementById('audStartStop');
-let audSkip = document.getElementById('audSkip')
+let audSkip = document.getElementById('audSkip');
 let sidebar = document.getElementById('sidebar');
 let expAction = document.getElementById('exp-action');
-let pointToBeGood = 0;
+let isRunning = false;
+let pointForLongRest = 0;
 let CirRoll = 0;
 let timeoutId;
+let timer;
+
 function startStop() {
-    audStartStop.play()
+    audStartStop.play();
     if (isRunning) {
         clearInterval(timer);
         isRunning = false;
-        playerPause.setAttribute('class','bi-caret-right')
+        playerPause.setAttribute('class','bi-caret-right');
     } else {
         timer = setInterval(countdown, 1000);
         isRunning = true;
-        playerPause.setAttribute('class','bi-pause')
-    }
-    
+        playerPause.setAttribute('class','bi-pause');
+    }    
 }
+
 // Reset action to be performed after 1 second
 document.getElementById('reset').addEventListener('mousedown', () => {
     timeoutId = setTimeout(() => {
@@ -55,63 +55,58 @@ function skip(){
     document.getElementById('time-left').innerText = '00:00';
 }
 
-
-
 function countdown() {
     let timeLeft = document.getElementById('time-left').innerText;
+    let audAlarm = document.getElementById('pomoAlarm')
     let [minutes, seconds] = timeLeft.split(':').map(Number);
+
+    // When you multiply this value with the minutes converted into seconds plus the seconds, 
+    // based on the longest possible minute, for example: 25 min, 15 min or 05 min, you get 440, 
+    // which is the value needed to make the complete wheel cycle
     let workingSecondToMakePerfectWheel = 0.29352901934623082054703135423616;
     let relaxingSecondToMakePerfectWheel = 1.4715719063545150501672240802676; 
     let beGoodSecondToMakePerfectWheel = 0.48943270300333704115684093437152;
     
-    let alarm = document.getElementById('pomoAlarm')
-    playerPause.setAttribute('class','bi-pause')
-    
+    playerPause.setAttribute('class','bi-pause')  
     if (minutes === 0 && seconds === 0) {
-        if (document.getElementById('timer-label').innerText === 'Working') {
-            
-            
-            if(pointToBeGood === 4){
+        if (document.getElementById('timer-label').innerText === 'Working') {    
+            if(pointForLongRest === 4){
                 document.getElementById('timer-label').innerText = 'Enjoy and rest';
                 timeLeft = '15:00';
                 wheel.style.stroke='#518E2B'
                 
-                //Clear all Cir
+                // reset the long rest indicator balls
                 for(let i=0;i<4;i++){
                     allCir[i].style.color='#ffffff77';
                 }
-                pointToBeGood=0
-                CirRoll = 0
-                if(pointToBeGood == 0){
-                    CirRoll = 0
-                }             
+                pointForLongRest=0
+                CirRoll = 0          
             }else{
+                // short rest
                 document.getElementById('timer-label').innerText = 'Relaxing';
                 timeLeft = '05:00';
                 wheel.style.stroke='#2B6A8E'
-                
                 allCir[CirRoll].style.color='#ffffff';
                 CirRoll++
-                pointToBeGood++;
+                pointForLongRest++;
             }
-       
-            alarm.play();
-  
+            audAlarm.play();
         } else {
             document.getElementById('timer-label').innerText = 'Working';
             timeLeft = '25:00';
             wheel.style.stroke='#8E672B'
-            alarm.play();
+            audAlarm.play();
             
         }
     } else {
+        // decrements minutes and seconds
         if (seconds === 0) {
             minutes--;
             seconds = 59;           
         } else {
             seconds--;
         }
-        let minutesInSeconds = minutes*60;
+        let minutesInSeconds = minutes*60; 
         let minutesPlusSecond = minutesInSeconds+seconds;
         if(document.getElementById('timer-label').innerText === 'Working'){
             wheel.style.strokeDashoffset = 440-(workingSecondToMakePerfectWheel*minutesPlusSecond);
